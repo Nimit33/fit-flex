fd5cef72a60064f814ae5030a676c4b9c8dd5e73
Nimit Gupta <gnimit146@gmail.com>
2024-11-17 10:20:13 +0530

    second-commit3

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -4,7 +4,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY, // <-- Trailing comma added
   },
 };
 
@@ -12,7 +12,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY, // <-- Trailing comma added
   },
 };
 
