6fab7f318e0d1315cbe8fc05cc96d1f643636d7d
Nimit Gupta <gnimit146@gmail.com>
2024-11-17 10:04:02 +0530

    second-commit2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
